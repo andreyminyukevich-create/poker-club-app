5067c76e2171066d68eb85305109f02924b5be08
andreyminyukevich-create <andreyminyukevich@gmail.com>
2026-03-03 21:07:28 +0300

    Delete frontend directory

--- frontend/src/api/tournaments.js
+++
@@ -1,9 +0,0 @@
-import api from './client';
-
-export function getAll() {
-  return api.get('/api/tournaments');
-}
-
-export function getById(id) {
-  return api.get('/api/tournaments/' + id);
-}
--- frontend/src/components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loader({ text }) {
-  return (
-    <div className="text-center mt-24 text-gray">
-      {text || '\u0417\u0430\u0433\u0440\u0443\u0437\u043A\u0430...'}
-    </div>
-  )
-}
