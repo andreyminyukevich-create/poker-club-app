57ee788434d64b2e689378992868c06e64755b30
andreyminyukevich-create <andreyminyukevich@gmail.com>
2026-03-04 18:53:10 +0300

    Create config.js

---
+++ frontend-2/src/config.js
@@ -0,0 +1,32 @@
+var IMG = 'https://buufaehdtlrriymyrgxh.supabase.co/storage/v1/object/public/image'
+
+export var IMAGES = {
+  chipRed: IMG + '/chip-red.png.png',
+  chipGreen: IMG + '/chip-green.png.png',
+  chipBlack: IMG + '/chip-black.png.png',
+  chipGold: IMG + '/chip-gold.png.png',
+  chipSilver: IMG + '/chip-silver.png.png',
+  bannerTournament: IMG + '/banner-tournament.png.png',
+  bannerRating: IMG + '/banner-rating.png.png',
+  bannerAbout: IMG + '/banner-about.png.png',
+  bgRed: IMG + '/bg-red.png.png',
+  bgGold: IMG + '/bg-gold.png.png',
+  bgPurple: IMG + '/bg-purple.png.png',
+  bgSilver: IMG + '/bg-silver.png.png',
+  bgBlue: IMG + '/bg-blue.png.png',
+  avatarDefault: IMG + '/avatar-default.png.png',
+  trophy: IMG + '/trophy.png.png',
+}
+
+// Rotate chips for tournament cards
+var CHIPS = [
+  IMG + '/chip-red.png.png',
+  IMG + '/chip-green.png.png',
+  IMG + '/chip-black.png.png',
+  IMG + '/chip-gold.png.png',
+  IMG + '/chip-silver.png.png',
+]
+
+export function getChipForTournament(index) {
+  return CHIPS[index % CHIPS.length]
+}
